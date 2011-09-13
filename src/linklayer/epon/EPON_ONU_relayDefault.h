753889f0e75af62e8702e00c3ccc22f7f749764d
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2011-09-13 14:49:05 +0100

    * Still working on it...

--- src/linklayer/epon/EPON_ONU_relayDefault.h
+++ src/linklayer/epon/EPON_ONU_relayDefault.h
@@ -1,17 +1,26 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-//
+///
+/// @file   EPON_ONU_relayDefault.h
+/// @author Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
+/// @date   Jun/30/2009
+///
+/// @brief  Declares 'OltScheduler' and its derived classes for a hybrid
+///			TDM/WDM-PON OLT.
+///
+/// @remarks Copyright (C) 2009-2010 Kyeong Soo (Joseph) Kim. All rights reserved.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Lesser General Public License for more details.
+///
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with this program.  If not, see http://www.gnu.org/licenses/.
+///
 
 #ifndef __EPON_RELAY_H__
 #define __EPON_RELAY_H__
